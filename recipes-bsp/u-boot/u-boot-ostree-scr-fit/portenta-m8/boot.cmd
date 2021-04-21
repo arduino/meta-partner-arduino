20c50339dcfdf00f7fbe84fdf7d7b1f2eacef5e9
Michael Scott <mike@foundries.io>
2021-04-21 14:52:52 -0700

    recipes-bsp: u-boot-ostree-scr-fit: support overlays var
    Let's allow user to select custom DTB overlays during the boot by
    setting a list of values for the "overlays" u-boot variable.
    
    Example eNuc setup in u-boot:
    setenv overlays 'ov_som_lbee5kl1dx \
      ov_carrier_enuc_bq24195 \
      ov_carrier_enuc_usbfs \
      ov_carrier_enuc_cs42l52 \
      ov_carrier_enuc_sara-r4 \
      ov_carrier_enuc_lora'
    
    Or from Linux command-line:
    sudo fw_setenv overlays 'ov_som_lbee5kl1dx \
      ov_carrier_enuc_bq24195 \
      ov_carrier_enuc_usbfs \
      ov_carrier_enuc_cs42l52 \
      ov_carrier_enuc_sara-r4 \
      ov_carrier_enuc_lora'
    
    Default value for overlays when not present is set to:
    ov_som_lbee5kl1dx
    
    Signed-off-by: Michael Scott <mike@foundries.io>

--- recipes-bsp/u-boot/u-boot-ostree-scr-fit/portenta-m8/boot.cmd
+++ recipes-bsp/u-boot/u-boot-ostree-scr-fit/portenta-m8/boot.cmd
@@ -4,16 +4,32 @@ echo "Using arduino_${fdt_file}"
 setenv bootlimit 3
 setenv devtype mmc
 setenv devnum 2
+setenv ovl_addr 0x43600000
 
 setenv bootcmd_resetvars 'setenv kernel_image; setenv bootargs; setenv kernel_image2; setenv bootargs2'
 setenv bootcmd_otenv 'run bootcmd_resetvars; ext4load ${devtype} ${devnum}:2 ${loadaddr} /boot/loader/uEnv.txt; env import -t ${loadaddr} ${filesize}'
 setenv bootcmd_load_f 'ext4load ${devtype} ${devnum}:2 ${loadaddr} "/boot"${kernel_image}'
-setenv bootcmd_run 'bootm ${loadaddr}#conf@arduino_${fdt_file}'
+setenv bootcmd_dtb 'imxtract ${loadaddr}#conf@arduino_${fdt_file} fdt@arduino_${fdt_file} ${fdt_addr}; fdt addr ${fdt_addr}'
+setenv bootcmd_overlay ' \
+  for ov in ${overlays}; do; \
+    if imxtract ${loadaddr}#conf@arduino_${fdt_file} fdt@${ov}.dtbo ${ovl_addr}; then \
+      fdt resize 0x${filesize}; \
+      fdt apply ${ovl_addr}; \
+      echo "Applied ${ov}.dtbo to DTB"; \
+    else \
+      echo "WARN: ${ov}.dtbo not found!"; \
+    fi; \
+  done'
+setenv bootcmd_run 'bootm ${loadaddr}#conf@arduino_${fdt_file} ${loadaddr}#conf@arduino_${fdt_file} ${fdt_addr}'
 setenv bootcmd_rollbackenv 'setenv kernel_image ${kernel_image2}; setenv bootargs ${bootargs2}'
 setenv bootcmd_set_rollback 'if test ! "${rollback}" = "1"; then setenv rollback 1; setenv upgrade_available 0; saveenv; fi'
-setenv bootostree 'run bootcmd_load_f; run bootcmd_run'
+setenv bootostree 'run bootcmd_load_f; run bootcmd_dtb; run bootcmd_overlay; run bootcmd_run'
 setenv altbootcmd 'run bootcmd_otenv; run bootcmd_set_rollback; if test -n "${kernel_image2}"; then run bootcmd_rollbackenv; fi; run bootostree; reset'
 
+# default overlays
+if env exist overlays; then true; else setenv overlays 'ov_som_lbee5kl1dx'; saveenv; fi
+
+# make sure env exists
 if test ! -e ${devtype} ${devnum}:1 uboot.env; then saveenv; fi
 
 if test "${rollback}" = "1"; then run altbootcmd; else run bootcmd_otenv; run bootostree; reset; fi
