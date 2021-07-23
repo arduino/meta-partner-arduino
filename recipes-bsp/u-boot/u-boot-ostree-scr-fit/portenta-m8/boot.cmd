0e247a68a51f6d1c965dcbf9c3edbd97b2a8650d
Michael Scott <mike@foundries.io>
2021-07-23 12:22:27 -0700

    recipes-bsp: u-boot-ostree-scr-fit: update boot.cmd for v82 changes
    Move Portenta-M8 to a copied version of the "common" boot script which
    handles boot software upgrades.
    
    Reason for copying is to handle the dtb overlay appliation.
    
    Signed-off-by: Michael Scott <mike@foundries.io>

--- recipes-bsp/u-boot/u-boot-ostree-scr-fit/portenta-m8/boot.cmd
+++ recipes-bsp/u-boot/u-boot-ostree-scr-fit/portenta-m8/boot.cmd
@@ -4,34 +4,23 @@ echo "Using arduino_${fdt_file}"
 setenv bootlimit 3
 setenv devtype mmc
 setenv devnum 2
+setenv bootpart 1
+setenv rootpart 2
 setenv ovl_addr 0x43600000
 
-setenv bootcmd_resetvars 'setenv kernel_image; setenv bootargs; setenv kernel_image2; setenv bootargs2'
-setenv bootcmd_otenv 'run bootcmd_resetvars; ext4load ${devtype} ${devnum}:2 ${loadaddr} /boot/loader/uEnv.txt; env import -t ${loadaddr} ${filesize}'
-setenv bootcmd_load_f 'ext4load ${devtype} ${devnum}:2 ${loadaddr} "/boot"${kernel_image}'
-setenv bootcmd_dtb 'imxtract ${loadaddr}#conf@@FIT_NODE_SEPARATOR@@arduino_${fdt_file} fdt@@FIT_NODE_SEPARATOR@@arduino_${fdt_file} ${fdt_addr}; fdt addr ${fdt_addr}'
-setenv bootcmd_overlay ' \
-  for ov in ${overlays}; do; \
-    if imxtract ${loadaddr}#conf@@FIT_NODE_SEPARATOR@@arduino_${fdt_file} fdt@@FIT_NODE_SEPARATOR@@${ov}.dtbo ${ovl_addr}; then \
-      fdt resize 0x${filesize}; \
-      fdt apply ${ovl_addr}; \
-      echo "Applied ${ov}.dtbo to DTB"; \
-    else \
-      echo "WARN: ${ov}.dtbo not found!"; \
-    fi; \
-  done'
-setenv bootcmd_run 'bootm ${loadaddr}#conf@@FIT_NODE_SEPARATOR@@arduino_${fdt_file} ${loadaddr}#conf@@FIT_NODE_SEPARATOR@@arduino_${fdt_file} ${fdt_addr}'
-setenv bootcmd_rollbackenv 'setenv kernel_image ${kernel_image2}; setenv bootargs ${bootargs2}'
-setenv bootcmd_set_rollback 'if test ! "${rollback}" = "1"; then setenv rollback 1; setenv upgrade_available 0; saveenv; fi'
-setenv bootostree 'run bootcmd_load_f; run bootcmd_dtb; run bootcmd_overlay; run bootcmd_run'
-setenv altbootcmd 'run bootcmd_otenv; run bootcmd_set_rollback; if test -n "${kernel_image2}"; then run bootcmd_rollbackenv; fi; run bootostree; reset'
+# Boot image files
+setenv fdt_file_final arduino_${fdt_file}
+setenv fit_addr ${initrd_addr}
 
-# default overlays
-if env exist overlays; then true; else setenv overlays 'ov_som_lbee5kl1dx'; saveenv; fi
+# Boot firmware updates
+setenv bootloader 42
+setenv bootloader2 300
+setenv bootloader_s 1042
+setenv bootloader2_s 1300
+setenv bootloader_image "imx-boot"
+setenv bootloader_s_image ${bootloader_image}
+setenv bootloader2_image "u-boot.itb"
+setenv bootloader2_s_image ${bootloader2_image}
+setenv uboot_hwpart 1
 
-# make sure env exists
-if test ! -e ${devtype} ${devnum}:1 uboot.env; then saveenv; fi
-
-if test "${rollback}" = "1"; then run altbootcmd; else run bootcmd_otenv; run bootostree; reset; fi
-
-reset
+@@INCLUDE_COMMON@@
